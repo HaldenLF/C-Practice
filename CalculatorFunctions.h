1de47bf9318c85617f95fa1df8969013234c6992
HaldenLF <165461117+HaldenLF@users.noreply.github.com>
2024-06-11 12:19:37 +0700

    Create CalculatorFunctions.h

---
+++ CalculatorFunctions.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+// headers for the calculator functions
+double Addition();
+double Subtraction();
+double Multiplication();
+double Division();
+
+#endif
